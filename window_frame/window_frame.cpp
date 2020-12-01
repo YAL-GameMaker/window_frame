c9587909cb69a6234b465861d266a16e4a7746d7
Samuel Joseph Venable <samuelvenable@hotmail.com>
2020-12-01 06:14:49 +0000

    add "fake" fullscreen (which allows for dialogs and embedded controls in fullscreen)

--- window_frame/window_frame.cpp
+++ window_frame/window_frame.cpp
@@ -218,6 +218,49 @@ dllx double window_frame_set_rect(double x, double y, double w, double h) {
 }
 #pragma endregion
 
+#pragma region Fake Fullscreen
+bool window_frame_fakefullscreen = false;
+dllx double window_frame_get_fakefullscreen() {
+	return window_frame_fakefullscreen;
+}
+
+RECT rectprev; RECT clientprev; LONG styleprev;
+bool window_frame_set_fakefullscreen_impl(bool full) {
+	if (full == window_frame_fakefullscreen) return true;
+	HWND hwnd = window_frame_game_hwnd;
+	HWND fwnd = window_frame_host_hwnd;
+	RECT rect = window_frame_get_rect();
+	//trace("%d,%d,%d,%d", rect.left, rect.top, rect.right, rect.bottom);
+	if (full) {
+		rectprev = rect;
+		GetClientRect(hwnd, &clientprev);
+		styleprev = GetWindowLong(fwnd, GWL_STYLE);
+		SetWindowLong(fwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_CHILD);
+		MoveWindow(fwnd, 0, 0,
+			GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN), true);
+		MoveWindow(hwnd, 0, 0,
+			GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN), true);
+	}
+	else {
+		SetWindowLong(fwnd, GWL_STYLE, styleprev & ~WS_CHILD);
+		SetWindowPos(fwnd, nullptr, 0, 0, 0, 0, SWP_NOSIZE);
+		AdjustWindowRect(&rectprev, GetWindowLong(fwnd, GWL_STYLE), false);
+		MoveWindow(fwnd, rectprev.left, rectprev.top, 
+			rectprev.right - rectprev.left, rectprev.bottom - rectprev.top, true);
+		SetWindowPos(hwnd, nullptr, 
+			(GetSystemMetrics(SM_CXSCREEN) / 2) - (clientprev.right / 2),
+			(GetSystemMetrics(SM_CYSCREEN) / 2) - (clientprev.bottom / 2),
+			clientprev.right, clientprev.bottom, 0);
+	}
+	SetFocus(hwnd);
+	window_frame_fakefullscreen = full;
+	return true;
+}
+dllx double window_frame_set_fakefullscreen(double full) {
+	return window_frame_set_fakefullscreen_impl(full > 0.5);
+}
+#pragma endregion
+
 #pragma region Window Caption
 ///
 dllx double window_frame_set_region(double x, double y, double w, double h) {
